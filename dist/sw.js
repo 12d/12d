6f788fb4eca803b490354c961db2c02f4e73d4ed
kangkangxiduomi <294837179@qq.com>
2018-04-27 14:56:35 +0800

    update2

--- dist/sw.js
+++ dist/sw.js
@@ -7,7 +7,6 @@ var __wpo = {
       "./static/js/manifest.js?69227fa7a0263b4f01e0",
       "./static/css/app.css",
       "./",
-      "https://bank-static.pingan.com.cn/*",
       "https://bank-static.pingan.com.cn/station/site/common/mobilehome/css/bank-module-public.css",
       "https://bank-static.pingan.com.cn/app_js/libs/zepto/1.2.0/zepto.min.js",
       "https://bank-static.pingan.com.cn/app_com/pab/1.0.0/pab.js",
@@ -18,7 +17,6 @@ var __wpo = {
     "optional": []
   },
   "externals": [
-    "https://bank-static.pingan.com.cn/*",
     "https://bank-static.pingan.com.cn/station/site/common/mobilehome/css/bank-module-public.css",
     "https://bank-static.pingan.com.cn/app_js/libs/zepto/1.2.0/zepto.min.js",
     "https://bank-static.pingan.com.cn/app_com/pab/1.0.0/pab.js",
@@ -35,7 +33,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "2018-4-27 14:53:36",
+  "version": "2018-4-27 14:55:45",
   "name": "webpack-offline",
   "pluginVersion": "4.9.0",
   "relativePaths": false,
