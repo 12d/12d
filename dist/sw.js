d28b940aba180971fe20aa6b005740f5d260c42f
kangkangxiduomi <294837179@qq.com>
2018-05-02 14:13:03 +0800

    sw update26

--- dist/sw.js
+++ dist/sw.js
@@ -24,7 +24,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": null,
+  "version": "2018-5-2 14:10:53",
   "name": "webpack-offline",
   "pluginVersion": "4.9.0",
   "relativePaths": false
@@ -169,7 +169,7 @@ self.addEventListener('fetch', function (event) {
     var IS_INDEX = /\/dist\/index./
     //var IS_PREVIEW = /^\/preview(?!\.)/
     var CACHE_PREFIX = __wpo.name
-    var CACHE_TAG = __wpo.pluginVersion
+    var CACHE_TAG = __wpo.version
     var CACHE_NAME = CACHE_PREFIX + ':' + CACHE_TAG
     var resource = undefined
     var isGET = event.request.method === 'GET'
