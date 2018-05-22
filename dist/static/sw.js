37ad6732b8f798d90274097217f8a0333e15c93a
kangkangxiduomi <294837179@qq.com>
2018-05-22 15:19:29 +0800

    update

--- dist/static/sw.js
+++ dist/static/sw.js
@@ -1,30 +1,30 @@
 var __wpo = {
   "assets": {
     "main": [
-      "./static/images/dialog-downloadApp.f4999d8.jpg",
-      "./static/js/app.js?22201f5d97153af89488",
-      "./static/js/vendor.js?85adec4df6352f787e3f",
-      "./static/js/manifest.js?6a301dfdd76272414855",
-      "./static/css/app.css",
-      "./",
-      "./static/sw-entry.js"
+      "https://12d.github.io/dist/static/images/dialog-downloadApp.f4999d8.jpg",
+      "https://12d.github.io/dist/static/js/app.js?22201f5d97153af89488",
+      "https://12d.github.io/dist/static/js/vendor.js?6334ebe32cf310a45453",
+      "https://12d.github.io/dist/static/js/manifest.js?6a301dfdd76272414855",
+      "https://12d.github.io/dist/static/css/app.css",
+      "https://12d.github.io/dist/",
+      "https://12d.github.io/dist/static/sw-entry.js"
     ],
     "additional": [],
     "optional": []
   },
   "externals": [],
   "hashesMap": {
-    "bc6edb3d5ea100b4e6eb7e18bd1055f34a814e75": "./static/images/dialog-downloadApp.f4999d8.jpg",
-    "9e89b1775ea1ffe2f1e6c1f7117ccabd465afd5e": "./static/js/app.js?22201f5d97153af89488",
-    "cc168a93d35fa489a3632d2b90c6e0da0356e5d1": "./static/js/vendor.js?85adec4df6352f787e3f",
-    "736fa9491c00b600c6f9f182223ebbb120817c0f": "./static/js/manifest.js?6a301dfdd76272414855",
-    "b7a0f99dd78c9cc02b056d164efc09ae16e2c39d": "./static/css/app.css",
-    "cac68f3039350113569d0fd9a59a8fa9e4164a20": "./",
-    "1ee6ec877af8c4f94e472579715db34e4216f7d9": "./static/sw-entry.js"
+    "bc6edb3d5ea100b4e6eb7e18bd1055f34a814e75": "https://12d.github.io/dist/static/images/dialog-downloadApp.f4999d8.jpg",
+    "9e89b1775ea1ffe2f1e6c1f7117ccabd465afd5e": "https://12d.github.io/dist/static/js/app.js?22201f5d97153af89488",
+    "eff16b34086002cf0da16daf9f70d74246fa374b": "https://12d.github.io/dist/static/js/vendor.js?6334ebe32cf310a45453",
+    "736fa9491c00b600c6f9f182223ebbb120817c0f": "https://12d.github.io/dist/static/js/manifest.js?6a301dfdd76272414855",
+    "b7a0f99dd78c9cc02b056d164efc09ae16e2c39d": "https://12d.github.io/dist/static/css/app.css",
+    "a1c8d35e4d17e19ec67bc4121ef7499c0839b5cd": "https://12d.github.io/dist/",
+    "1ee6ec877af8c4f94e472579715db34e4216f7d9": "https://12d.github.io/dist/static/sw-entry.js"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "2018-5-22 14:56:12",
+  "version": "2018-5-22 15:18:12",
   "name": "webpack-offline",
   "pluginVersion": "4.9.1",
   "relativePaths": false
